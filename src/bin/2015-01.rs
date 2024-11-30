cedfa648060ad226f3953a43a9c715d65e13a056
Meadow Liu <meadowsys@kiwin.gay>
2024-11-29 21:57:18 -0800

    2015 day 1

--- src/bin/2015-01.rs
+++ src/bin/2015-01.rs
@@ -1,3 +1,34 @@
+#![no_implicit_prelude]
+extern crate wiwi;
+use wiwi::aoc::prelude::*;
+
 fn main() {
-	println!("uwu");
+	let input = get_input(2015, 1);
+
+	// part 1
+	let floor = input
+		.chars()
+		.map(get_value)
+		.sum::<isize>();
+	print_p1(floor);
+
+	let mut val = 0isize;
+	let floor = input
+		.chars()
+		.enumerate()
+		.find(|(i, c)| {
+			val += get_value(*c);
+			val.is_negative()
+		})
+		.unwrap()
+		.0;
+	print_p2(floor + 1);
+}
+
+fn get_value(c: char) -> isize {
+	match c {
+		'(' => { 1 }
+		')' => { -1 }
+		_ => { unreachable!() }
+	}
 }
---
+++ src/bin/2015-02.rs
@@ -0,0 +1,10 @@
+#![no_implicit_prelude]
+
+// extern crate wiwi;
+
+// use wiwi::aoc::prelude::*;
+
+fn main() {
+	// println!("uwu");
+	// println!("{}", file!());
+}
