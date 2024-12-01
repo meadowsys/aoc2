56fa293f14a7bc55e55dafe86c9287ccc3fa9bc8
Meadow Liu <meadowsys@kiwin.gay>
2024-12-01 01:59:15 -0800

    2019 day 2 pt 2

--- src/bin/2019-02.rs
+++ src/bin/2019-02.rs
@@ -2,6 +2,8 @@
 extern crate wiwi;
 use wiwi::aoc::prelude::*;
 use wiwi::chain::*;
+use std::thread;
+use atomic::Ordering::*;
 
 fn main() {
 	let input = get_input(2019, 2);
@@ -11,22 +13,58 @@ fn main() {
 	let mut p1 = input.clone();
 	p1[1] = 12;
 	p1[2] = 2;
+	run_intcode(&mut p1);
+	print_p1(p1[0]);
+
+	// let's try some multithreaded atomics programming for part 2 because why not
+	let result = AtomicUsize::new(0);
+	let mut ranges = [0usize..20, 20..40, 40..60, 60..80, 80..100];
+	let expected = 19690720usize;
+	thread::scope(|s| {
+		for range in &mut ranges {
+			s.spawn(|| {
+				for noun in range {
+					for verb in 0..100 {
+						// does this actually have to do acquire/release? or can this
+						// all be relaxed, cause nothing is like, producing new values
+						// from values written from past threads or anything depending
+						// on past writes
+						if result.load(Acquire) != 0 { return }
+
+						let mut input = input.clone();
+						input[1] = noun;
+						input[2] = verb;
+						run_intcode(&mut input);
+
+						let output = input[0];
+						if output == expected {
+							result.store(100 * noun + verb, Release);
+						}
+					}
+				}
+			});
+		}
+	});
+
+	print_p2(result.into_inner());
+}
+
+fn run_intcode(memory: &mut [usize]) {
 	let mut pos = 0;
 	loop {
-		match p1[pos] {
+		match memory[pos] {
 			1 => {
-				let (l, r, out) = (p1[pos + 1], p1[pos + 2], p1[pos + 3]);
-				p1[out] = p1[l] + p1[r];
+				let (l, r, out) = (memory[pos + 1], memory[pos + 2], memory[pos + 3]);
+				memory[out] = memory[l] + memory[r];
 				pos += 4;
 			}
 			2 => {
-				let (l, r, out) = (p1[pos + 1], p1[pos + 2], p1[pos + 3]);
-				p1[out] = p1[l] * p1[r];
+				let (l, r, out) = (memory[pos + 1], memory[pos + 2], memory[pos + 3]);
+				memory[out] = memory[l] * memory[r];
 				pos += 4;
 			}
 			99 => { break }
 			_ => { unreachable!() }
 		}
 	}
-	print_p1(p1[0]);
 }
