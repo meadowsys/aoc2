059bce0d516203b2557fb51808716055051b1f44
Meadow Liu <meadowsys@kiwin.gay>
2024-12-01 01:05:03 -0800

    2019 day 1 pt 2
    l o n g

--- src/bin/2019-01.rs
+++ src/bin/2019-01.rs
@@ -8,8 +8,14 @@ fn main() {
 
 	let p1 = input.trim().lines().map(|l| l.parse().unwrap()).map(fuel_for).sum::<usize>();
 	print_p1(p1);
+	let p2 = input.trim().lines().map(|l| l.parse().unwrap()).map(fuel_for_recurse).sum::<usize>();
+	print_p2(p2);
 }
 
 fn fuel_for(mass: usize) -> usize {
 	(mass / 3) - 2
+}
+
+fn fuel_for_recurse(mass: usize) -> usize {
+	(mass / 3).checked_sub(2).map(|m| m + fuel_for_recurse(m)).unwrap_or_default()
 }
