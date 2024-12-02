3c0eb01a276846836cdc23f76fd894691888ae14
Meadow Liu <meadowsys@kiwin.gay>
2024-12-01 21:53:28 -0800

    2024 day 2 pt 2
    I hate it lol

--- src/bin/2024-02.rs
+++ src/bin/2024-02.rs
@@ -1,23 +1,36 @@
 #![no_implicit_prelude]
-#![feature(array_windows)]
+#![feature(array_windows, iter_map_windows)]
 extern crate wiwi;
 use wiwi::aoc::prelude::*;
 
 fn main() {
 	let input = get_input(2024, 2);
 
-	let p1 = input.lines().filter(|line| {
-		let vec = line.split(' ').map(|num| num.parse().unwrap()).collect::<Vec<usize>>();
+	let input = input.lines().map(|line| line.split(' ').map(|num| num.parse().unwrap()).collect::<Vec<usize>>()).collect::<Vec<_>>();
 
-		let all_inc = vec.array_windows::<2>().all(|[a, b]| a < b);
-		let all_dec = vec.array_windows::<2>().all(|[a, b]| a > b);
-		if !(all_inc || all_dec) { return false }
-
-		let op: fn(usize, usize) -> usize = if all_inc { |a, b| b - a } else { |a, b| a - b };
-		// let sm_diff = vec.array_windows::<2>().all
-		vec.array_windows::<2>().all(|[a, b]| {
-			(1..=3).contains(&op(*a, *b))
-		})
-	}).count();
+	let p1 = input.iter().filter(|vec| is_safe(vec.iter().copied()).unwrap_or(true)).count();
 	print_p1(p1);
+
+	let p2 = input.iter().filter(|vec| (0..vec.len()).any(|del| is_safe(vec.iter().copied().enumerate().filter(|(i, _)| *i != del).map(|(_, n)| n)).unwrap_or(true))).count();
+	print_p2(p2);
+
+}
+
+fn is_safe<I>(iter: I) -> Option<bool>
+where
+	I: IntoIterator<Item = usize>
+{
+	let mut iter = iter.into_iter().peekable();
+
+	let a = iter.next()?;
+	let b = *iter.peek()?;
+	let op = if a < b {
+		|a, b| a < b && (1..=3).contains(&(b - a))
+	} else {
+		|a, b| b < a && (1..=3).contains(&(a - b))
+	};
+
+	if !op(a, b) { return Some(false) }
+
+	Some(iter.map_windows::<_, _, 2>(|[a, b]| op(*a, *b)).all(identity))
 }
